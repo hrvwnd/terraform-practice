1ff7cd14c969ccc4a6b5e18590430561d9d0a3be
hrvwnd <harveyawendon@gmail.com>
2020-01-29 15:33:26 +0000

    adding gcp related terraform

---
+++ task-gcp/main.tf
@@ -0,0 +1,28 @@
+
+
+provider "google" {
+	credentials = "${file("~/.config/gcloud/application_default_credentials.json")}"
+	project = "fleet-pillar-258809"
+  	region  = "europe-west2"
+  	zone    = "europe-west2-a"
+}
+
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network       = "default"
+    access_config {
+    }
+  }
+}
+
